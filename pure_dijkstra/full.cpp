eac55a1f0294c40a7c3a0f0b8862b8a7838d1f4f
keyraphi <keyraphi@gmail.com>
2022-02-22 14:31:39 +0100

    Fixed some compiler warnings

--- pure_dijkstra/full.cpp
+++ pure_dijkstra/full.cpp
@@ -1,12 +1,11 @@
 // Patrick Wieschollek, 2016
-#include <malloc.h>
 #include <array>
 #include <cstdlib>
 #include <iostream>
+#include <malloc.h>
 #include <vector>
 
-template <class T>
-inline constexpr T pow8(const T base) {
+template <class T> inline constexpr T pow8(const T base) {
   return (((base * base) * (base * base)) * ((base * base) * (base * base)));
 }
 
@@ -14,26 +13,30 @@ inline constexpr T pow8(const T base) {
 python setup.py clean && python setup.py build && python setup.py install --user
 */
 
-template <typename T>
-class Mat {
-    private:
-        bool is_data_allocated;
- public:
-  T* data;
+template <typename T> class Mat {
+public:
+  T *data;
   int width, height;
 
-  Mat(T* d, const int h, const int w) : data(d), width(w), height(h), is_data_allocated(false) {}
+private:
+  bool is_data_allocated;
+
+public:
+  Mat(T *d, const int h, const int w)
+      : data(d), width(w), height(h), is_data_allocated(false) {}
   Mat() : width(0), height(0), is_data_allocated(false) {}
-  Mat(const int h, const int w) : width(w), height(h), is_data_allocated(true) { data = new T[w * h]; }
+  Mat(const int h, const int w) : width(w), height(h), is_data_allocated(true) {
+    data = new T[w * h];
+  }
   ~Mat() {
-      if (is_data_allocated) {
-          delete[] data;
-      }
+    if (is_data_allocated) {
+      delete[] data;
+    }
   }
 
-  inline T& operator()(int x, int y) { return data[x * width + y]; }
+  inline T &operator()(int x, int y) { return data[x * width + y]; }
 
-  inline const T& operator()(int x, int y) const { return data[x * width + y]; }
+  inline const T &operator()(int x, int y) const { return data[x * width + y]; }
 
   const int col_arg_min(int y) {
     T min_value = data[0 * width + y];
@@ -54,10 +57,10 @@ class Dijkstra {
   Mat<float> cost;
   int m, n;
 
- public:
-  float* latest_prop;
+public:
+  float *latest_prop;
 
-  Dijkstra(float* cost_, int m_, int n_) : m(m_), n(n_) {
+  Dijkstra(float *cost_, int m_, int n_) : m(m_), n(n_) {
     // std::cout << "[Dijkstra]   load cost of size " << m_<< " x "<< n_<<
     // std::endl;
     cost = Mat<float>(cost_, m_, n_);
@@ -73,9 +76,9 @@ class Dijkstra {
    *
    * @return box-corners (top, bottom, left, right)
    */
-  std::vector<std::array<int, 4> > find_boxes(
-      std::vector<std::array<int, 2> >& tour, int max_frames = 400) {
-    std::vector<std::array<int, 4> > boxes;
+  std::vector<std::array<int, 4>>
+  find_boxes(std::vector<std::array<int, 2>> &tour, int max_frames = 400) {
+    std::vector<std::array<int, 4>> boxes;
     int r_last, r_start, s_last, s_start;
     r_last = r_start = tour[0][0];
     s_last = s_start = tour[0][1];
@@ -113,11 +116,11 @@ class Dijkstra {
    *
    * @return all found tour points sorted(!)
    */
-  std::vector<std::array<int, 2> > multiple_tours(int stripe_len = 250,
-                                                  int frame_offset = 30) {
+  std::vector<std::array<int, 2>> multiple_tours(int stripe_len = 250,
+                                                 int frame_offset = 30) {
     // 83.33 = 250*10 / 30 --> 2*60*30/10 = 360.00  (2min)
-    std::vector<std::array<int, 2> > single_tour;
-    std::vector<std::vector<std::array<int, 2> > > multi_tour;
+    std::vector<std::array<int, 2>> single_tour;
+    std::vector<std::vector<std::array<int, 2>>> multi_tour;
 
     const int overlap = stripe_len / 2;
     // overlap = 0;
@@ -163,21 +166,25 @@ class Dijkstra {
     }
 
     // std::cout << "alive04 " << single_tour.size() << std::endl;
-    std::vector<std::array<int, 2> > result;
+    std::vector<std::array<int, 2>> result;
     for (unsigned int i = 0, i_e = single_tour.size(); i < i_e; ++i) {
       const float c1 = pow8(cost(single_tour[i][0], single_tour[i][1]));
       float c2 = 0;
       int x_off = 50;
       int y_off = 50;
 
-      if (single_tour[i][0] > m / 2) x_off = -50;
-      if (single_tour[i][1] > n / 2) y_off = -50;
+      if (single_tour[i][0] > m / 2)
+        x_off = -50;
+      if (single_tour[i][1] > n / 2)
+        y_off = -50;
       c2 = pow8(cost(single_tour[i][0] + x_off, single_tour[i][1] + y_off));
 
-      if (c1 / c2 < 0.89) result.push_back(single_tour[i]);
+      if (c1 / c2 < 0.89)
+        result.push_back(single_tour[i]);
     }
     // std::cout << "alive05" << std::endl;
-    if ((unsigned int)n / 4 > result.size()) result.clear();
+    if ((unsigned int)n / 4 > result.size())
+      result.clear();
 
     return result;
   }
@@ -195,9 +202,9 @@ class Dijkstra {
    * endpoints)
    * @return [description]
    */
-  std::vector<std::array<int, 2> > tour(
-      const int off = 0, const int col_end = 0, const int overlap_info = 0,
-      const int id_info = 0, const int max_info = 0, const bool ssp = false) {
+  std::vector<std::array<int, 2>>
+  tour(const int off = 0, const int col_end = 0, const int overlap_info = 0,
+       const int id_info = 0, const int max_info = 0, const bool ssp = false) {
     int cut_start = 0;
     int cut_end = col_end;
 
@@ -221,7 +228,8 @@ class Dijkstra {
     }
 
     // information about the start
-    for (int r = 0; r < m; ++r) accumulate_costs(r, 0) = pow8(cost(r, off));
+    for (int r = 0; r < m; ++r)
+      accumulate_costs(r, 0) = pow8(cost(r, off));
     if (ssp) {
       // we have to start on the top left corner
       // accumulate_costs(0, 0) = 0;
@@ -259,10 +267,10 @@ class Dijkstra {
     latest_prop = accumulate_costs.data;
 
     // // find tour
-    std::vector<std::array<int, 2> > t;
+    std::vector<std::array<int, 2>> t;
     t.clear();
 
-    int column = col_end;  // column
+    int column = col_end; // column
     int row = accumulate_costs.col_arg_min(col_end - off - 1);
     if (ssp) {
       // we have to end on the bottom right corner
@@ -273,20 +281,21 @@ class Dijkstra {
     // <<std::endl;
 
     if (column > cut_start)
-      if (column < cut_end) t.push_back({row, column});
+      if (column < cut_end)
+        t.push_back({row, column});
 
     while ((row > 0) && (column > off + 1)) {
       switch (directions(row, column - off - 1)) {
-        case 1:  // from top
-          row--;
-          break;
-        case 2:  // from top-left
-          column--;
-          row--;
-          break;
-        case 3:  // from left
-          column--;
-          break;
+      case 1: // from top
+        row--;
+        break;
+      case 2: // from top-left
+        column--;
+        row--;
+        break;
+      case 3: // from left
+        column--;
+        break;
       }
 
       if ((column > cut_start) && (column < cut_end))
@@ -296,21 +305,21 @@ class Dijkstra {
   }
 };
 
-void advanced_dijkstra(int width, int frame_accurarcy, float* cost, int m,
-                       int n, int** tour, int* tour_len, int** box,
-                       int* box_len) {
+void advanced_dijkstra(int width, int frame_accurarcy, float *cost, int m,
+                       int n, int **tour, int *tour_len, int **box,
+                       int *box_len) {
   Dijkstra alg(cost, m, n);
-  std::vector<std::array<int, 2> > tour_vec =
+  std::vector<std::array<int, 2>> tour_vec =
       alg.multiple_tours(width, frame_accurarcy);
 
   // std::cout << "found "<< tour_vec.size() <<" tour entries" << std::endl;
   int tour_len_ = tour_vec.size() * 2;
-  int* tour_;
+  int *tour_;
 
   if (tour_len_ == 0) {
-    tour_ = (int*)malloc(1 * sizeof(int));
+    tour_ = (int *)malloc(1 * sizeof(int));
   } else {
-    tour_ = (int*)malloc(tour_len_ * sizeof(int));
+    tour_ = (int *)malloc(tour_len_ * sizeof(int));
     for (unsigned int i = 0, i_e = tour_vec.size(); i < i_e; i++) {
       tour_[2 * i] = tour_vec[i][0];
       tour_[2 * i + 1] = tour_vec[i][1];
@@ -323,16 +332,16 @@ void advanced_dijkstra(int width, int frame_accurarcy, float* cost, int m,
   ////////////////////////////////////////
   // std::cout << "get boxes" << std::endl;
   int box_len_ = 0;
-  int* box_;
+  int *box_;
   if (tour_len_ == 0) {
     box_len_ = 1;
-    box_ = (int*)malloc(box_len_ * sizeof(int));
+    box_ = (int *)malloc(box_len_ * sizeof(int));
 
   } else {
-    std::vector<std::array<int, 4> > box_vec = alg.find_boxes(tour_vec);
+    std::vector<std::array<int, 4>> box_vec = alg.find_boxes(tour_vec);
 
     box_len_ = box_vec.size() * 4;
-    box_ = (int*)malloc(box_len_ * sizeof(int));
+    box_ = (int *)malloc(box_len_ * sizeof(int));
 
     for (unsigned int i = 0, i_e = box_vec.size(); i < i_e; i++) {
       box_[4 * i] = box_vec[i][0];
@@ -346,13 +355,13 @@ void advanced_dijkstra(int width, int frame_accurarcy, float* cost, int m,
   *box_len = box_len_;
 }
 
-void plain_dijkstra(int width, int frame_accurarcy, float* cost, int m, int n,
-                    int** tour, int* tour_len) {
+void plain_dijkstra(int width, int frame_accurarcy, float *cost, int m, int n,
+                    int **tour, int *tour_len) {
   Dijkstra alg(cost, m, n);
-  std::vector<std::array<int, 2> > tour_vec = alg.tour(0, n, 0, 0, 0, true);
+  std::vector<std::array<int, 2>> tour_vec = alg.tour(0, n, 0, 0, 0, true);
 
   int tour_len_ = tour_vec.size() * 2;
-  int* tour_ = (int*)malloc(tour_len_ * sizeof(int));
+  int *tour_ = (int *)malloc(tour_len_ * sizeof(int));
 
   for (unsigned int i = 0, i_e = tour_vec.size(); i < i_e; i++) {
     tour_[2 * i] = tour_vec[i][0];
@@ -363,9 +372,9 @@ void plain_dijkstra(int width, int frame_accurarcy, float* cost, int m, int n,
   *tour_len = tour_len_;
 }
 
-void accumulate(float* cost, int m, int n) {
+void accumulate(float *cost, int m, int n) {
   Dijkstra alg(cost, m, n);
-  std::vector<std::array<int, 2> > tour_vec = alg.tour(0, n, 0, 0, 0, true);
+  std::vector<std::array<int, 2>> tour_vec = alg.tour(0, n, 0, 0, 0, true);
 
   for (unsigned int i = 0; i < m * n; ++i) {
     cost[i] = alg.latest_prop[i];
